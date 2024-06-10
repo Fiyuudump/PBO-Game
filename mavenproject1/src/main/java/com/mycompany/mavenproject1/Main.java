70a93af2784a7672be87d6ad7f686daab9278696
D-19 <D-19@D-19>
2024-06-10 20:30:47 +0700

    inpoi

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author D-19
+ */
+
+public class Main {
+    public static void main(String[] args){
+     Game game = new Game(3);
+     game.play();
+    }
+}
